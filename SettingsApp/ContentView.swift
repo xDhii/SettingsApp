36eb7715f3d971b73ff657d04f9a34d2c725f6ab
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2024-01-11 20:35:40 -0300

    Initial Commit

---
+++ SettingsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SettingsApp
+//
+//  Created by Adriano Valumin on 11/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SettingsApp/SettingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsAppApp.swift
+//  SettingsApp
+//
+//  Created by Adriano Valumin on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SettingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
