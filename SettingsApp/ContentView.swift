a40341beb86a79cff202731639ee28abd9856e58
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2024-01-13 20:55:50 -0300

    v0.1
    Created first version of settings screen clone, with a design pattern for first components.

--- SettingsApp/ContentView.swift
+++ SettingsApp/ContentView.swift
@@ -8,14 +8,171 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var search: String = ""
+    @State var connectedWifi = "Valumin"
+    let wifiList = ["Valumin", "Valumin_5G"]
+    let bluetoothList = ["Adriano's AirPods Pro", "Adriano's AirPods Max"]
+    let enabledDisabled = ["Ativado", "Desativado"]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Form {
+                    Section {
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                                .foregroundStyle(.secondary)
+
+                            TextField("Buscar", text: $search)
+                                .font(.callout)
+
+                            Image(systemName: "mic.fill")
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+
+                    Section {
+                        IcloudProfileItem(userName: "Adriano Valumin",
+                                          profilePic: "adriano")
+                    }
+
+                    Section {
+                        ItemNavigationLink(icon: "airpodsmax",
+                                           name: "Adriano's AirPods Max",
+                                           backgroundColor: .orange,
+                                           lightFont: true)
+                    }
+
+                    Section {
+                        ItemToggle(toggle: false,
+                                   icon: "airplane",
+                                   name: "Modo Avião",
+                                   backgroundColor: .orange)
+
+                        ItemNavigationLinkPicker(selectedItem: wifiList[0],
+                                                 icon: "wifi",
+                                                 name: "Wi-Fi",
+                                                 backgroundColor: .blue,
+                                                 itemsList: wifiList)
+
+                        ItemNavigationLinkPicker(selectedItem: bluetoothList[0],
+                                                 icon: "questionmark",
+                                                 name: "Bluetooth",
+                                                 backgroundColor: .blue,
+                                                 itemsList: bluetoothList)
+
+                        ItemNavigationLink(icon: "antenna.radiowaves.left.and.right",
+                                           name: "Celular",
+                                           backgroundColor: .green)
+
+                        ItemNavigationLinkPicker(selectedItem: enabledDisabled[0],
+                                                 icon: "personalhotspot",
+                                                 name: "Acesso Pessoal",
+                                                 backgroundColor: .green,
+                                                 itemsList: enabledDisabled)
+                    }
+
+                    Section {
+                        ItemNavigationLink(icon: "bell.badge.fill",
+                                           name: "Notificações",
+                                           backgroundColor: Color(red: 0.992,
+                                                                  green: 0.267,
+                                                                  blue: 0.222))
+
+                        ItemNavigationLink(icon: "speaker.wave.3.fill",
+                                           name: "Som e Tato",
+                                           backgroundColor: Color(red: 1.0,
+                                                                  green: 0.21,
+                                                                  blue: 0.369))
+
+                        ItemNavigationLink(icon: "moon.fill",
+                                           name: "Foco",
+                                           backgroundColor: Color(red: 0.365,
+                                                                  green: 0.357,
+                                                                  blue: 0.902))
+
+                        ItemNavigationLink(icon: "questionmark",
+                                           name: "Tempo de Uso",
+                                           backgroundColor: Color(red: 0.365,
+                                                                  green: 0.357,
+                                                                  blue: 0.902))
+                    }
+
+                    Section {
+                        ItemNavigationLink(icon: "gear",
+                                           name: "Geral",
+                                           backgroundColor: .gray)
+
+                        ItemNavigationLink(icon: "questionmark",
+                                           name: "Central de Controle",
+                                           backgroundColor: .gray)
+
+                        ItemNavigationLink(icon: "sun.max.fill",
+                                           name: "Tela e Brilho",
+                                           backgroundColor: .blue)
+
+                        ItemNavigationLink(icon: "questionmark",
+                                           name: "Tela de Início e Biblioteca de Apps",
+                                           backgroundColor: Color(red: 0.366,
+                                                                  green: 0.357,
+                                                                  blue: 0.902))
+
+                        ItemNavigationLink(icon: "accessibility",
+                                           name: "Acessibilidade",
+                                           backgroundColor: Color(red: -0.003,
+                                                                  green: 0.478,
+                                                                  blue: 0.999))
+
+                        ItemNavigationLink(icon: "questionmark",
+                                           name: "Imagem de Fundo",
+                                           backgroundColor: Color(red: 0.184,
+                                                                  green: 0.68,
+                                                                  blue: 0.899))
+
+                        ItemNavigationLink(icon: "questionmark",
+                                           name: "Em Espera",
+                                           backgroundColor: .black)
+
+                        ItemNavigationLink(icon: "questionmark",
+                                           name: "Siri e Busca",
+                                           backgroundColor: .blue)
+
+                        ItemNavigationLink(icon: "faceid",
+                                           name: "Face ID e Código",
+                                           backgroundColor: Color(red: 0.18,
+                                                                  green: 0.82,
+                                                                  blue: 0.345))
+
+                        ItemNavigationLink(icon: "sos",
+                                           name: "SOS de Emergência",
+                                           backgroundColor: Color(red: 0.995,
+                                                                  green: 0.267,
+                                                                  blue: 0.222))
+
+                        ItemNavigationLink(icon: "sun.min.fill",
+                                           name: "Notificações de Exposição",
+                                           backgroundColor: .white,
+                                           foregroundColor: .red,
+                                           border: true)
+
+                        ItemNavigationLink(icon: "battery.100percent",
+                                           name: "Bateria",
+                                           backgroundColor: Color(red: 0.18,
+                                                                  green: 0.82,
+                                                                  blue: 0.345))
+
+                        ItemNavigationLink(icon: "hand.raised.fill",
+                                           name: "Privacidade e Segurança",
+                                           backgroundColor: Color(red: 0.043,
+                                                                  green: 0.518,
+                                                                  blue: 1.001))
+                    }
+
+                    Section("Tests") { }
+                }
+                .navigationTitle("Ajustes")
+            }
         }
-        .padding()
     }
 }
 
---
+++ SettingsApp/FormItemsListView.swift
@@ -0,0 +1,152 @@
+//
+//  FormItemsListView.swift
+//  SettingsApp
+//
+//  Created by Adriano Valumin on 13/01/24.
+//
+
+import SwiftUI
+
+struct FormItemsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IcloudProfileItem: View {
+    var userName: String
+    var profilePic: String
+    var navigationLinkContent: (any View)?
+
+    var body: some View {
+        HStack {
+            NavigationLink {
+                if let navigationLinkContent = navigationLinkContent {
+                    AnyView(navigationLinkContent)
+                } else {
+                    Text("Screen to be developed.")
+                }
+            } label: {
+                Image(profilePic)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 48)
+                    .clipShape(Circle())
+
+                VStack {
+                    HStack {
+                        Text("\(userName)")
+                            .font(.title2)
+                        Spacer()
+                    }
+
+                    HStack {
+                        Text("ID Apple, iCloud+, Mídia e Compras")
+                            .font(.caption)
+                        Spacer()
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ItemNavigationLinkPicker: View {
+    @State var selectedItem: String = "DefaultValue"
+    var icon: String
+    var name: String
+    var backgroundColor: Color
+    var foregroundColor: Color = .white
+    var itemsList: [String]
+
+    var body: some View {
+        HStack {
+                Image(systemName: icon)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.horizontal, 5)
+                    .padding(.vertical, 4)
+                    .frame(width: 30, height: 30)
+                    .foregroundStyle(foregroundColor)
+                    .background(backgroundColor)
+                    .clipShape(RoundedRectangle(cornerRadius: 7))
+                
+                Picker(name, selection: $selectedItem) {
+                    ForEach(itemsList, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.navigationLink)
+        }
+    }
+}
+
+struct ItemNavigationLink: View {
+    var icon: String
+    var name: String
+    var backgroundColor: Color
+    var foregroundColor: Color = .white
+    var itemsList: [String]?
+    var lightFont: Bool = false
+    var border: Bool = false
+
+    var body: some View {
+        HStack {
+            Image(systemName: icon)
+                .resizable()
+//                .frame(width: 18, height: 18)
+                .scaledToFit()
+                .padding(.horizontal, 5)
+                .padding(.vertical, 4)
+                .frame(width: 30, height: 30)
+                .foregroundStyle(foregroundColor)
+                .background(backgroundColor)
+                .clipShape(RoundedRectangle(cornerRadius: 7))
+                .overlay(border ? RoundedRectangle(cornerRadius: 10).stroke(Color.secondary, lineWidth: 1).opacity(0.3) : nil)
+
+            NavigationLink {
+                Form {
+                    if let itemsList = itemsList {
+                        ForEach(itemsList, id: \.self) {
+                            Text($0)
+                        }
+                        .navigationTitle(name)
+                        .toolbarTitleDisplayMode(.inline)
+                    } else { }
+                }
+            } label: {
+                Text(name)
+            }
+            .fontWeight(lightFont ? .light : .regular)
+        }
+    }
+}
+
+struct ItemToggle: View {
+    @State var toggle = true
+    var icon: String
+    var name: String
+    var backgroundColor: Color = .orange
+    var foregroundColor: Color = .white
+
+    var body: some View {
+        HStack {
+            Image(systemName: icon)
+                .resizable()
+                .frame(width: 18, height: 18)
+                .scaledToFit()
+                .padding(.horizontal, 5)
+                .padding(.vertical, 4)
+                .frame(width: 30, height: 30)
+                .foregroundStyle(foregroundColor)
+                .background(backgroundColor)
+                .clipShape(RoundedRectangle(cornerSize: CGSize(width: 6, height: 5)))
+            Toggle(name, isOn: $toggle)
+        }
+    }
+}
+
+#Preview {
+//    FormItemsListView()
+    IcloudProfileItem(userName: "Adriano", profilePic: "adriano")
+}
